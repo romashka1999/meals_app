047e63341233372ab1663b4ddd92a531890d0dae
Your Namect git config --global user.email <roma.chikhladze777@gmail.com>
2020-05-23 00:39:19 +0400

    set up

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+
+
+const loadFonts = () => {
+    return Font.loadAsync({
+        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    });
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+    const [fontsLoaded, setFontsLoaded] = useState(false);
+
+    if(!fontsLoaded) {
+        return (
+            <AppLoading 
+                startAsync={loadFonts}
+                onFinish={() => setFontsLoaded(true)}
+                onError={(err) => console.log(err)}/>
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+
+const FavoritesScreen = (props) => {
+    return (  
+        <View style={styles.screen}>
+            <Text>The favorites</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
+ 
+export default FavoritesScreen;
